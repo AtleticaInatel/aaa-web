393bb0397540c76e39be8fc1eacce29be97498f0
João Tavares <joaostavares@outlook.com>
2022-12-24 14:58:17 -0300

    Start of project, there's a lot of things to do

---
+++ components/linkProfile.js
@@ -0,0 +1,10 @@
+
+function Profile() {
+    return (
+        <div className='w-full pt-12 pb-4
+        flex justify-center items-center
+        flex-col text-center space-y-6'>
+
+        </div>
+    );
+}
