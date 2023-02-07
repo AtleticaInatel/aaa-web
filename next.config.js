1bdb0f89f6a7a1e1cdc0af9cc00853d7f0791f4e
João Tavares <joaostavares@outlook.com>
2023-02-07 16:07:39 -0300

    - Redir to store

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
             "https://forms.gle/Upw27e22hVk6dM7R8",
         permanent: true,
       },
+      {
+        source: "/loja",
+        destination:
+            "https://www.cheersapp.com.br/atl%C3%A9tica-inatel~196",
+        permanent: true,
+      },
     ];
   },
 }
