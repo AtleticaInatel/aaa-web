d40d6f52209361a0317320644703b1ee0bc1afdc
Atlética Inatel <129891438+AtleticaInatel@users.noreply.github.com>
2023-04-04 15:21:51 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,24 +5,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/interpanelas",
-        destination:
-            "https://forms.gle/YooC5FXJaFYFBYp78",
-        permanent: true,
-      },
-      {
-        source: "/mkt",
-        destination:
-            "https://forms.gle/Upw27e22hVk6dM7R8",
-        permanent: true,
-      },
-      {
-        source: "/geral",
-        destination:
-            "https://forms.gle/dXGq6aTcX6ZXzX627",
-        permanent: true,
-      },
-      {
         source: "/loja",
         destination:
             "https://www.cheersapp.com.br/atl%C3%A9tica-inatel~196",
